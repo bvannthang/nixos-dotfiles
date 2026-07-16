5ae3b2eeea5d7160e17bdb4fd484c936c01fcbec
Bui Van Thang <b.vann.thang@gmail.com>
2026-07-16 11:02:24 +0700

    setup: init setup

---
+++ home.nix
@@ -0,0 +1,34 @@
+{ config, pkgs, ... }:
+
+let
+  dotfiles = "${config.home.homeDirectory}/nixos-dotfiles/config";
+  create_symlink = path: config.lib.file.mkOutOfStoreSymlink path;
+
+  configs = {
+    alacritty = "alacritty";
+    fish      = "fish";
+    git       = "git";
+    i3        = "i3";
+    i3status  = "i3status";
+    nvim      = "nvim";
+  };
+in
+
+{
+  home.username = "buivanthang";
+  home.homeDirectory = "/home/buivanthang";
+  home.stateVersion = "26.05";
+
+  programs.home-manager.enable = true;
+
+  xdg.configFile = builtins.mapAttrs
+    (name: subpath: {
+      source = create_symlink "${dotfiles}/${subpath}";
+      recursive = true;
+    })
+    configs;
+
+  home.packages = with pkgs; [
+    alacritty
+  ];
+}
